5b350f547d870d2cadb698d7545b3daca0235a68
Sajeenth Shivauthayalingam <sajeenth.shivauthayalingam@systemswithintelligence.com>
2025-12-10 11:39:00 -0800

    deploy to prod

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from youreka import create_app
 
-# You can switch to ProdConfig via env var if needed.
-app = create_app("DevConfig")
+app = create_app("ProdConfig")
 
 if __name__ == "__main__":
-    app.run()
+    # Local dev
+    app.run(debug=True)
